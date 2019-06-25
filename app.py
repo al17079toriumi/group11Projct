8820695b66197564ed98c2a733f9e9eb4a015ff5
RYoTA <ryota-develop@outlook.com>
2019-06-25 14:18:37 +0900

    change MYSQL_USER.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import bcrypt
 app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'kenpa'
+app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'qazwsx'
 app.config['MYSQL_DB'] = 'USERDB'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
